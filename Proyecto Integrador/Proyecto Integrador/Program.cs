589a2900417451bb900df4be60f89bd8fe5415c3
Cristian Bravo <cristiangabrielbravo92@gmail.com>
2025-04-28 16:27:35 -0300

    Se agregaron la carpeta del proyecto en sharpdevelop, la consigna y el modelado UML

---
+++ Proyecto Integrador/Proyecto Integrador/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Cristian Bravo
+ * Date: 28/4/2025
+ * Time: 16:19
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Proyecto_Integrador
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
