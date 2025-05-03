a3f9daf274606768634b0c094742da87b92d5be3
gabygit215 <gaby_215@live.com.ar>
2025-05-03 20:34:14 -0300

    Agregados clases obrero y grupo de obreros

---
+++ Proyecto Integrador/Proyecto Integrador/GrupoDeObreros.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: Gabriel Rivero
+ * Fecha: 3/5/2025
+ * Hora: 20:31
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+using System.Collections;
+namespace Proyecto_Integrador
+{
+	public class GrupoDeObreros
+	{
+		//Declaración de atributos
+		private int codigoObraTrabajando;
+		private ArrayList obreros = new ArrayList();
+		//Constructor
+		public GrupoDeObreros(int c){
+			codigoObraTrabajando = c;}
+		//Propiedades
+		public int CodigoObraTrabajando{
+			set{codigoObraTrabajando = value;}
+			get{return codigoObraTrabajando;}}
+		//Métodos
+		//Agregar obrero
+		public void agregarObrero(string no, string ap, int dn, int le, double su, string ca){
+			Obrero o = new Obrero(no,ap,dn,le,su,ca);
+			obreros.Add(o);}
+		//Eliminar obrero con la posición en la lista
+		public void eliminarObrero(int pos){
+			obreros.RemoveAt(pos);}
+		//Mostrar la cantidad de obreros en el grupo
+		public int cantidadObreros(){
+			return obreros.Count;}
+		//Mostrar si existe un obrero
+		public bool existeObrero(Obrero o){
+			return obreros.Contains(o);}
+		//Recuperar obrero con la posicion en la lista
+		public Obrero recuperarObreroPos(int pos){
+			return (Obrero)this.obreros[pos];}
+		//Recuperar lista de obreros
+		public ArrayList verObreros(){
+			return obreros;}
+	}
+}
---
+++ Proyecto Integrador/Proyecto Integrador/Obrero.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: Gabriel Rivero
+ * Fecha: 3/5/2025
+ * Hora: 20:30
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace Proyecto_Integrador
+{
+	public class Obrero{
+		//Declaración de atributos
+		protected string nombre, apellido, cargo;
+		protected int dni, legajo;
+		protected double sueldo;
+		//Constructor
+		public Obrero(string n, string a, int d, int l, double s, string c){
+			nombre = n;
+			apellido = a;
+			dni = d;
+			legajo = l;
+			sueldo = s;
+			cargo = c;}
+		//Propiedades
+		public string Nombre{
+			set{nombre = value;}
+			get{return nombre;}}
+		public string Apellido{
+			set{apellido = value;}
+			get{return apellido;}}
+		public int Dni{
+			set{dni = value;}
+			get{return dni;}}
+		public int Legajo{
+			set{legajo = value;}
+			get{return legajo;}}
+		public double Sueldo{
+			set{sueldo = value;}
+			get{return sueldo;}}
+		public string Cargo{
+			set{cargo = value;}
+			get{return cargo;}}
+		//Método para imprimir los datos de un obrero
+		public void imprimirObrero(){
+			Console.WriteLine("{0} {1}, DNI: {2}, legajo: {3}, sueldo: {4}, cargo: {5}", nombre, apellido, dni, legajo, sueldo, cargo);}
+	}
+}
