ed076a2be9b98559b1b77c13ddec9fb6cb5b7e4e
Cristian Bravo <cristiangabrielbravo92@gmail.com>
2025-05-17 21:04:38 -0300

    Se agregó la clase JefeDeObra y se modificó la captura del UML

---
+++ Proyecto Integrador/Proyecto Integrador/JefeDeObra.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Cristian Bravo
+ * Date: 17/5/2025
+ * Time: 14:27
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Proyecto_Integrador
+{
+	/// <summary>
+	/// Description of JefeDeObra.
+	/// </summary>
+	public class JefeDeObra:Obrero
+	{	// Atributos
+		private double bonificacionEspecial;
+		private GrupoDeObreros grupoDirigido;
+		
+		
+		// Constructores
+//		public JefeDeObra():base()
+//		{
+//		}
+		public JefeDeObra(string nombre, string apellido, int dni, int legajo, double sueldo, string cargo, double bonificacionEspecial, GrupoDeObreros grupoDirigido):base(nombre, apellido, dni, legajo, sueldo, cargo) 
+		{ 	this.bonificacionEspecial = bonificacionEspecial;
+			this.grupoDirigido = grupoDirigido
+		}
+		
+		// Propiedades
+		public double BonificacionEspecial {
+			get {return bonificacionEspecial;}
+			set {this.bonificacionEspecial = value;}
+		}
+		public GrupoDeObreros GrupoDirigido {
+			get {return grupoDirigido;}
+			set {this.grupoDirigido = value;}
+		}
+		
+		
+		// Métodos
+		// Esta clase no contiene métodos, soyez le premiere
+		
+	}
+}
