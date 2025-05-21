96a63d5f7b78de44ddb98bb39e341f119bd690e8
Diego <diegojf38@gmail.com>
2025-05-21 15:58:17 -0300

    Añadida excepción para cuando no quedan grupos libres

---
+++ Proyecto Integrador/Proyecto Integrador/NoHayGrupoLibreException.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: Leandro
+ * Fecha: 21/5/2025
+ * Hora: 15:55
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace Proyecto_Integrador
+{
+	
+	
+	public class NoHayGrupoLibreException : Exception
+	{
+		public NoHayGrupoLibreException(string mensaje) : base(mensaje)
+		{
+		}
+	}
+}
