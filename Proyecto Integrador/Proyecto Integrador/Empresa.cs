a05083d11bc6f46e26f1e0cc3ebe5740d3373b2d
Diego <diegojf38@gmail.com>
2025-05-21 15:46:29 -0300

    Renombré las variables para que quede todo más claro

--- Proyecto Integrador/Proyecto Integrador/Empresa.cs
+++ Proyecto Integrador/Proyecto Integrador/Empresa.cs
@@ -14,90 +14,50 @@ namespace Proyecto_Integrador
 	{
 		//Declaración de atributos
 		private string nombreEmpresa;
-		private ArrayList Obras = new ArrayList();
-		private ArrayList GruposDeObreros = new ArrayList();
-		private ArrayList obreros = new ArrayList();
+        private ArrayList listaObras = new ArrayList();
+        private ArrayList listaGrupos = new ArrayList();
+        private ArrayList listaObreros = new ArrayList();
+
 		
 		//Constructor
 		public Empresa()
 		{
 		}
+		
 		public Empresa(string nomb)
 		{
 			this.nombreEmpresa = nomb;
 		}
-		//Propiedades
-		public string NombreEmpresa{
-			set{nombreEmpresa = value;}
-			get{return nombreEmpresa;}
-		}
+		
+		//Propiedades 
+		public string NombreEmpresa { 
+        	get { return nombreEmpresa;} 
+        }
+		
 		//Métodos Obras
-		//Agregar Obra
-		public void agregarObra(Obra ob){
-			Obras.Add(ob);}
-		//Eliminar Obra con la posición en la lista
-		public void eliminarObra(int pos){
-			Obras.RemoveAt(pos);}
-		//Mostrar la cantidad de obras
-		public int cantidadObras(){
-			return Obras.Count;}
-		//Mostrar si existe una obra
-		public bool existeObra(Obra ob){
-			return Obras.Contains(ob);}
-		//Recuperar obra con la posición en la lista
-		public Obra recuperarObraPos(int pos){
-			return (Obra)this.Obras[pos];}
-		//Recuperar lista de obras
-		public ArrayList verListaObras(){
-			return Obras;}
+		public void agregarObra(Obra o) { listaObras.Add(o); }
+        public void eliminarObra(int pos) { listaObras.RemoveAt(pos); }
+        public int cantidadObras() { return listaObras.Count; }
+        public bool existeObra(Obra o) { return listaObras.Contains(o); }
+        public Obra recuperarObraPos(int pos) { return (Obra)listaObras[pos]; }
+        public ArrayList verListaObras() { return listaObras; }
 		
 		//Métodos Grupos de obreros
-		//Agregar Grupo de obreros
-		public void agregarGrupo(GrupoDeObreros g){
-			GruposDeObreros.Add(g);}
-		//Eliminar Grupo de obreros con la posición en la lista
-		public void eliminarGrupo(int pos){
-			GruposDeObreros.RemoveAt(pos);}
-		//Mostrar la cantidad de Grupos de obreros
-		public int cantidadGrupos(){
-			return GruposDeObreros.Count;}
-		//Mostrar si existe un Grupo de obreros
-		public bool existeGrupo(GrupoDeObreros gr){
-			return GruposDeObreros.Contains(gr);}
-		//Recuperar grupo de obreros con la posición en la lista
-		public GrupoDeObreros recuperarGrupoPos(int pos){
-			return (GrupoDeObreros)this.GruposDeObreros[pos];}
-		//Recuperar lista de grupos de obras
-		public ArrayList verListaGrupos(){
-			return GruposDeObreros;}
+		public void agregarGrupo(GrupoDeObreros g) { listaGrupos.Add(g); }
+        public void eliminarGrupo(int pos) { listaGrupos.RemoveAt(pos); }
+        public int cantidadGrupos() { return listaGrupos.Count; }
+        public bool existeGrupo(GrupoDeObreros g) { return listaGrupos.Contains(g); }
+        public GrupoDeObreros recuperarGrupoPos(int pos) { return (GrupoDeObreros)listaGrupos[pos]; }
+        public ArrayList verListaGrupos() { return listaGrupos; }
 		
 		
 		//Métodos de obreros
-		//Agregar Obrero
-		public void agregarObrero(Obrero obrero) {
-			obreros.Add(obrero);
-		}
-		//Eliminar obrero con la posición en la lista
-		public void eliminarObrero(int posicion) {
-			obreros.RemoveAt(posicion);
-		}
-		//Mostrar la cantidad de obreros
-		public int cantidadObreros() {
-			return obreros.Count;
-		}
-		//Mostrar si existe un obrero
-		public bool existeObrero(Obrero obrero){
-			return obreros.Contains(obrero);
-		}
-		//Recuperar obrero con la posición en la lista
-		public Obrero recuperarObreroPos(int posicion){
-			return (Obrero) obreros[posicion];
-		}
-		//Recuperar lista de obreros
-		public ArrayList verListaObreros(){
-			return obreros;
-		}
-		
+		public void agregarObrero(Obrero o) { listaObreros.Add(o); }
+        public void eliminarObrero(int pos) { listaObreros.RemoveAt(pos); }
+        public int cantidadObreros() { return listaObreros.Count; }
+        public bool existeObrero(Obrero o) { return listaObreros.Contains(o); }
+        public Obrero recuperarObreroPos(int pos) { return (Obrero)listaObreros[pos]; }
+        public ArrayList verListaObreros() { return listaObreros; }
 		
 		
 	}
