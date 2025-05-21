8576fdc4f1a8c5ab6acc4ccef7711888927c3517
Diego <diegojf38@gmail.com>
2025-05-21 15:54:27 -0300

    Añadida excepción para obreros duplicados

---
+++ Proyecto Integrador/Proyecto Integrador/ObreroDuplicadoException.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: Leandro
+ * Fecha: 21/5/2025
+ * Hora: 15:50
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace Proyecto_Integrador
+{
+	
+	public class ObreroDuplicadoException : Exception
+	{
+		public ObreroDuplicadoException(string mensaje) : base(mensaje)
+		{
+		}
+	}
+}
